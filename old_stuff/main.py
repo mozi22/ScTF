3f88adb185079dbe2dde249874ff6c731e40afea
muazzam <muazzamalii@hotmail.com>
2018-01-17 18:30:17 +0100

    changes

---
+++ old_stuff/main.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+import run_network
+import helpers
+
+# # read data
+# img1 = Image.open(os.path.join(examples_dir,'sculpture1.png'))
+# img2 = Image.open(os.path.join(examples_dir,'sculpture2.png'))
+
+# input_data = prepare_input_data(img1,img2,data_format)
+
+# print(get_available_gpus())
+prefix = '../../dataset_synthetic_sm/'
+
+# with tf.name_scope("datareader"):
+filenames = [prefix+'driving_TRAIN.tfrecords']
+reader = run_network.DatasetReader()
+reader.main(filenames)
+# print(helpers.readPFM('0006.pfm'))
