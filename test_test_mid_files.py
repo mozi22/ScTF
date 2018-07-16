5ab006d0f9b4930701b573faf75f7288ec20639b
Ali Muazzam <muazzama@dacky.informatik.uni-freiburg.de>
2018-07-16 18:37:36 +0200

    added color flow predictions for optical flow

--- test_test_mid_files.py
+++ test_test_mid_files.py
@@ -3,13 +3,14 @@ import losses_helper
 import tensorflow as tf
 import network
 import numpy as np
+from math import pi
 import ijremote as ij
 ij.setHost('tcp://linus:13463')
 
 def get_network_input_forward(image_batch,label_batch):
     return image_batch[:,0,:,:,:], label_batch[:,0,:,:,:]
- 
-ds = 'ptb'
+
+ds = 'flying'
 filee = ['../dataset_synthetic/'+ds+'_TEST.tfrecords']
 
 
@@ -18,9 +19,89 @@ test_dataset = data_reader.read_with_dataset_api_test(1,filee,version='1')
 test_iterator = test_dataset.make_one_shot_iterator()
 
 
+def create_flow_color_legend(size=64):
+    """Creates the flow color code legend
+
+    size: int
+        Number of pixels for width and height
+
+    Returns the rgb tensor with the image
+    """
+    X, Y = np.meshgrid(np.linspace(-1,1,64),np.linspace(-1,1,64))
+    flow = np.concatenate((X[np.newaxis,:,:],Y[np.newaxis,:,:]))
+    flow_tensor = tf.constant(flow[np.newaxis,:,:,:], dtype=tf.float32)
+    return create_flow_color_image(flow_tensor)[0]
+
+
+
+def create_flow_color_image(flow, denormalize=False, max_length=None, name=None):
+    """Creates a color coded optical flow image with the magnitude as saturation
+    and the hue as direction.
+    White pixels indicate nonfinite values in the x or y component.
+    
+    flow: Tensor 
+        Tensor in NCHW format with C=2. The first channel is the x component
+    
+    denormalize: bool
+        If True denormalized the optical flow with the image dimensions
+        
+    max_length: Tensor or None
+        This value is used to normalize the flow vectors.
+        If None each batch item will be normalized individually by the largest
+        flow vector.
+        
+    Returns a flow rgb image in NHWC format with C=3 and the vector of values used for normalization
+    """
+    with tf.name_scope(name, 'create_flow_color_image', [flow]) as scope:
+        # flow = tf.convert_to_tensor(flow, name='flow')
+        shape = flow.get_shape().as_list()
+        assert len(shape)==4
+        assert shape[1]==2
+        if not max_length is None:
+            max_length = tf.convert_to_tensor(max_length, name='max_length',dtype=flow.dtype)
+            max_length.get_shape().with_rank(1)
+            max_length_shape = max_length.get_shape().as_list()
+            assert shape[0] == max_length_shape[0] or 1 == max_length_shape[0]
+            if max_length_shape[0] == 1:
+                max_length = tf.tile(max_length,[shape[0]])
+
+        width = shape[-1]
+        height = shape[-2]
+
+        flow_nhwc = tf.transpose(flow,[0,2,3,1])
+
+        fx, fy = tf.split(flow_nhwc, num_or_size_splits=2, axis=-1)
+        if denormalize:
+            fx *= width
+            fy *= height
+
+        veclength = tf.sqrt(fx**2+fy**2)
+            
+        mask = tf.is_finite(veclength)
+        veclength_clean = tf.where(mask, veclength, tf.zeros_like(veclength))
+        if max_length is None:
+            max_veclength = tf.reduce_max(veclength_clean, axis=[1,2,3],keep_dims=True)
+        else:
+            max_veclength = tf.reshape(max_length, [shape[0],1,1,1])
+            
+        veclength_clean_norm = veclength_clean/max_veclength
+        angle = tf.atan2(fy, fx)
+
+        print(angle,mask, veclength_clean_norm,max_veclength)
+
+        hue = tf.where(mask, (angle+pi)/(2*pi), tf.zeros_like(veclength))
+        sat = tf.where(mask, tf.ones_like(veclength), tf.zeros_like(veclength))
+        val = tf.where(mask, veclength_clean_norm, tf.ones_like(veclength))
+        hsv = tf.clip_by_value(tf.concat([hue,sat,val], axis=-1), 0, 1)
+        rgb = tf.image.hsv_to_rgb(hsv)
+        print('agya2')
+        print(max_veclength)
+
+    return rgb
+
 def load_model_ckpt(sess,filename):
-	saver = tf.train.Saver()
-	saver.restore(sess, tf.train.latest_checkpoint(filename))
+    saver = tf.train.Saver()
+    saver.restore(sess, tf.train.latest_checkpoint(filename))
 
 def further_resize_imgs_lbls(network_input_images,network_input_labels):
 
@@ -93,7 +174,6 @@ total_loss = losses_helper.endpoint_loss(Y_forward,flows_dict['predict_flow'][0]
 
 test_image_batch, test_label_batch, filename1, filename2 = test_iterator.get_next()[0]
 
-
 summaies = []
 
 img_forward = tf.concat([X_forward[:,:,:,0:3],X_forward[:,:,:,3:6]],axis=2)
@@ -128,7 +208,12 @@ img_fb_predict = tf.concat([tf.expand_dims(flows_dict['predict_flow'][0][:,:,:,0
 warped_img = losses_helper.flow_warp(X_forward[:,:,:,3:6],denormalized_flow)
 
 
+flow_color_legend = create_flow_color_legend()
+flow_color_image = create_flow_color_image(tf.transpose(denormalized_flow,[0,3,1,2]))
+
 summaies.append(tf.summary.image('warped_img',tf.concat([X_forward[:,:,:,0:3],warped_img],axis=2)))
+summaies.append(tf.summary.image('flow_color_legend',tf.expand_dims(flow_color_legend,axis=0)))
+summaies.append(tf.summary.image('flow_color_image',flow_color_image))
 summaies.append(tf.summary.image('image_forward',img_forward))
 summaies.append(tf.summary.image('image_backward',img_backward))
 # summaies.append(tf.summary.image('depth_forward',depth_forward))
@@ -154,26 +239,31 @@ load_model_ckpt(sess,'ckpt/driving/network_with_fb_optical_flow/train/')
 
 test_summary_writer = tf.summary.FileWriter('./testboard/'+ds, sess.graph)
 
-
 step = 0
 total_losser = 0
 while True:
 
     print('iteration '+str(step))
+
     try:
         test_image_batch_fine, test_label_batch_fine, filenamee1, filenamee2 = sess.run([test_image_batch, test_label_batch, filename1, filename2])
 
+
         summary_str_test, total_loss2,denormalize_f,Y_forwardd,X_forwardd = sess.run([summary_op,total_loss,denormalized_flow,Y_forward,X_forward],feed_dict={
-            X: test_image_batch_fine,
-            Y: test_label_batch_fine
+                    X: test_image_batch_fine,
+                    Y: test_label_batch_fine
         })
 
-        step+=1
-        total_losser+=total_loss2
-
+        step += 1
+        total_losser += total_loss2
     except tf.errors.OutOfRangeError:
-        print(total_losser/step)
-        print('finished')
+        print('finish')
+        avg = total_losser / step
+        print(avg)
+        break
+
+
+    if step == 5:
         break
 
     # print(filenamee1)
@@ -182,7 +272,7 @@ while True:
     # ij.setImage('normal_lbl',np.transpose(Y_forwardd[:,:,:,:],[0,3,1,2]))
     # ij.setImage('prediction',np.transpose(denormalize_f,[0,3,1,2]))
 
-    # test_summary_writer.add_summary(summary_str_test, i)
+    test_summary_writer.add_summary(summary_str_test, step)
 
 
 test_summary_writer.close()
